0ca39a8d2df1d62a8e8db0b82d41c04a158a87da
nathanlafrogne <nathan.lafrogne@gmail.com>
2023-12-05 15:18:03 +0100

    age rating business

---
+++ VideoTheque/Businesses/Genres/AgeRatingBusiness.cs
@@ -0,0 +1,58 @@
+﻿using VideoTheque.Core;
+using VideoTheque.DTOs;
+using VideoTheque.Repositories.AgeRating;
+
+namespace VideoTheque.Businesses.Genres
+{
+    public class AgeRatingBusiness
+    {
+        
+        private readonly IAgeRatingsRepository _ageRatingDao;
+
+        public AgeRatingBusiness(IAgeRatingsRepository ageRatingDao)
+        {
+            _ageRatingDao = ageRatingDao;
+        }
+
+        public Task<List<AgeRatingDto>> GetAgeRatings() => _ageRatingDao.GetAgesRating();
+
+        public AgeRatingDto GetAgeRating(int id)
+        {
+            var ar = _ageRatingDao.GetAgeRating(id).Result;
+
+            if (ar == null)
+            {
+                throw new NotFoundException($"Age Rating '{id}' non trouvé");
+            }
+
+            return ar;
+        }
+
+        public AgeRatingDto InsertAgeRating(AgeRatingDto arDTO)
+        {
+            if (_ageRatingDao.InsertAgeRating(arDTO).IsFaulted)
+            {
+                throw new InternalErrorException($"Erreur lors de l'insertion du genre {arDTO.Name}");
+            }
+
+            return arDTO;
+        }
+
+        public void UpdateAgeRating(int id, AgeRatingDto arDTO)
+        {
+            if (_ageRatingDao.UpdateAgeRating(id, arDTO).IsFaulted)
+            {
+                throw new InternalErrorException($"Erreur lors de la modification du genre {arDTO.Name}");
+            }
+        }
+                
+
+        public void DeleteAgeRating(int id)
+        {
+            if (_ageRatingDao.DeleteAgeRating(id).IsFaulted)
+            {
+                throw new InternalErrorException($"Erreur lors de la suppression du genre d'identifiant {id}");
+            }
+        }
+    }
+}
---
+++ VideoTheque/Businesses/Genres/IAgeRatingBusiness.cs
@@ -0,0 +1,18 @@
+﻿using VideoTheque.DTOs;
+
+namespace VideoTheque.Businesses.Genres
+{
+    public interface IAgeRatingBusiness
+    {
+        Task<List<GenreDto>> GetAgesRating();
+
+        GenreDto GetAgeRating(int id);
+
+        GenreDto InsertAgeRating(AgeRatingDto genre);
+
+        void UpdateAgeRating(int id, AgeRatingDto genre);
+
+        void DeleteAgeRating(int id);
+        
+    }
+}
