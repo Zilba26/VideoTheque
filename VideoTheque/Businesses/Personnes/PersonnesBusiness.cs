532623844a9bc6528a8b2c8713793571db2b6dc8
Zilba26 <basile.lebaudy@gmail.com>
2023-12-05 17:11:06 +0100

    personnes business added

---
+++ VideoTheque/Businesses/Personnes/PersonnesBusiness.cs
@@ -0,0 +1,56 @@
+﻿using VideoTheque.Core;
+using VideoTheque.DTOs;
+using VideoTheque.Repositories.Personnes;
+
+namespace VideoTheque.Businesses.Personnes
+{
+    public class PersonnesBusiness
+    {
+        private readonly IPersonnesRepository _personnesDao;
+        
+        public PersonnesBusiness(IPersonnesRepository personnesDao)
+        {
+            _personnesDao = personnesDao;
+        }
+        
+        public Task<List<PersonneDto>> GetPersonnes() => _personnesDao.GetPersonnes();
+        
+        public PersonneDto GetPersonne(int id)
+        {
+            var personne = _personnesDao.GetPersonne(id).Result;
+            
+            if (personne == null)
+            {
+                throw new NotFoundException($"Personne '{id}' non trouvée");
+            }
+            
+            return personne;
+        }
+        
+        public PersonneDto InsertPersonne(PersonneDto personne)
+        {
+            if (_personnesDao.InsertPersonne(personne).IsFaulted)
+            {
+                throw new InternalErrorException($"Erreur lors de l'insertion de la personne {personne.LastName} {personne.FirstName}");
+            }
+            
+            return personne;
+        }
+        
+        public void UpdatePersonne(int id, PersonneDto personne)
+        {
+            if (_personnesDao.UpdatePersonne(id, personne).IsFaulted)
+            {
+                throw new InternalErrorException($"Erreur lors de la modification de la personne {personne.LastName} {personne.FirstName}");
+            }
+        }
+        
+        public void DeletePersonne(int id)
+        {
+            if (_personnesDao.DeletePersonne(id).IsFaulted)
+            {
+                throw new InternalErrorException($"Erreur lors de la suppression de la personne d'identifiant {id}");
+            }
+        }
+    }
+}
