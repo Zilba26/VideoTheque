62ba9082e1bcbe027c5e31dd3ffeac3572aa0103
Zilba26 <basile.lebaudy@gmail.com>
2023-12-05 14:49:47 +0100

    Age ratings repository added

---
+++ VideoTheque/Repositories/AgeRating/IAgeRatingsRepository.cs
@@ -0,0 +1,17 @@
+﻿using VideoTheque.DTOs;
+
+namespace VideoTheque.Repositories.AgeRating
+{
+    public interface IAgeRatingsRepository
+    {
+        Task<List<AgeRatingDto>> GetAgesRating();
+        
+        ValueTask<AgeRatingDto?> GetAgeRating(int id);
+        
+        Task InsertAgeRating(AgeRatingDto ageRating);
+        
+        Task UpdateAgeRating(int id, AgeRatingDto ageRating);
+        
+        Task DeleteAgeRating(int id);
+    }
+}
