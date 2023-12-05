6c1df65b6bad1dc8564bf4f1e608184251dfd47b
Zilba26 <basile.lebaudy@gmail.com>
2023-12-05 17:17:56 +0100

    personne controller added

---
+++ VideoTheque/Controllers/PersonnesController.cs
@@ -0,0 +1,47 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using VideoTheque.Businesses.Personnes;
+using VideoTheque.DTOs;
+using VideoTheque.ViewModels;
+
+namespace VideoTheque.Controllers
+{
+    [ApiController]
+    [Route("personnes")]
+    public class PersonnesController
+    {
+        private readonly IPersonnesBusiness _personnesBusiness;
+        
+        public PersonnesController(IPersonnesBusiness personnesBusiness)
+        {
+            _personnesBusiness = personnesBusiness;
+        }
+        
+        [HttpGet]
+        public async Task<List<PersonneViewModel>> GetPersonnes() => (await _personnesBusiness.GetPersonnes()).Adapt<List<PersonneViewModel>>();
+        
+        [HttpGet("{id}")]
+        public async Task<PersonneViewModel> GetPersonne([FromRoute] int id) => _personnesBusiness.GetPersonne(id).Adapt<PersonneViewModel>();
+        
+        [HttpPost]
+        public async Task<IResult> InsertPersonne([FromBody] PersonneViewModel personneVM)
+        {
+            var created = _personnesBusiness.InsertPersonne(personneVM.Adapt<PersonneDto>());
+            return Results.Created($"/personnes/{created.Id}", created);
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<IResult> UpdatePersonne([FromRoute] int id, [FromBody] PersonneViewModel personneVM)
+        {
+            _personnesBusiness.UpdatePersonne(id, personneVM.Adapt<PersonneDto>());
+            return Results.NoContent();
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<IResult> DeletePersonne([FromRoute] int id)
+        {
+            _personnesBusiness.DeletePersonne(id);
+            return Results.Ok();
+        }
+    }
+}
---
+++ VideoTheque/ViewModels/PersonneViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using VideoTheque.DTOs;
+
+namespace VideoTheque.ViewModels
+{
+    public class PersonneViewModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        
+        [JsonProperty("lastname")]
+        public string Lastname { get; set; }
+        
+        [JsonProperty("firstname")]
+        public string Firstname { get; set; }
+        
+        [JsonProperty("nationality")]
+        public string Nationality { get; set; }
+        
+        [JsonProperty("birthday")]
+        public DateTime Birthday { get; set; }
+        
+
+
+        public PersonneDto ToDto()
+        {
+            return new PersonneDto
+            {
+                Id = this.Id,
+                LastName = this.Lastname,
+                FirstName = this.Firstname,
+                Nationality = this.Nationality,
+                BirthDay = this.Birthday,
+            };
+        }
+
+        public static PersonneViewModel ToModel(PersonneDto dto)
+        {
+            return new PersonneViewModel
+            {
+                Id = dto.Id,
+                Lastname = dto.LastName,
+                Firstname = dto.FirstName,
+                Birthday = dto.BirthDay,
+                Nationality = dto.Nationality
+            };
+        }
+    }
+}
