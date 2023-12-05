50eb44923b8b1dd0f7540fac8623a5311bea6fe8
Zilba26 <basile.lebaudy@gmail.com>
2023-12-05 15:23:10 +0100

    Add age ratings controller

---
+++ VideoTheque/ViewModels/AgeRatingViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using VideoTheque.DTOs;
+
+namespace VideoTheque.ViewModels
+{
+    public class AgeRatingViewModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        
+        [JsonProperty("nom")]
+        [Required]
+        public string Name { get; set; }
+        
+        [JsonProperty("abreviation")]
+        public string Abreviation { get; set; }
+
+        public AgeRatingDto ToDto()
+        {
+            return new AgeRatingDto
+            {
+                Id = this.Id,
+                Name = this.Name,
+                Abreviation = this.Abreviation
+            };
+        }
+        
+        public static AgeRatingViewModel ToModel(AgeRatingDto dto)
+        {
+            return new AgeRatingViewModel
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Abreviation = dto.Abreviation
+            };
+        }
+    }
+}
