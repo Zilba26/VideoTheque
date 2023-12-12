3e04d4d8aa29ca4721f103272dc8caefdf281d82
nathanlafrogne <nathan.lafrogne@gmail.com>
2023-12-12 11:21:55 +0100

    feat : add bluray repository

---
+++ VideoTheque/Repositories/Film/BluRayRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoTheque.Context;
+using VideoTheque.DTOs;
+
+namespace VideoTheque.Repositories.Film
+{
+    public class BluRayRepository : IBluRayRepository
+    {
+
+        private readonly VideothequeDb _db;
+
+        public BluRayRepository(VideothequeDb db)
+        {
+            _db = db;
+        }
+
+        public Task<List<BluRayDto>> GetBluRays() => _db.BluRays.ToListAsync();
+
+        public ValueTask<BluRayDto?> GetBluRay(int id) => _db.BluRays.FindAsync(id);
+
+        public Task InsertBluRay(BluRayDto film)
+        {
+            _db.BluRays.AddAsync(film);
+            return _db.SaveChangesAsync();
+        }
+
+        public Task UpdateBluRay(int id, BluRayDto film)
+        {
+            var filmToUpdate = _db.BluRays.FindAsync(id).Result;
+
+            if (filmToUpdate is null)
+            {
+                throw new KeyNotFoundException($"Film '{id}' non trouvé");
+            }
+
+            filmToUpdate.Title = film.Title;
+            // TODO
+            return _db.SaveChangesAsync();
+        }
+
+        public Task DeleteBluRay(int id)
+        {
+            var filmToDelete = _db.BluRays.FindAsync(id).Result;
+
+            if (filmToDelete is null)
+            {
+                throw new KeyNotFoundException($"Film '{id}' non trouvé");
+            }
+
+            _db.BluRays.Remove(filmToDelete);
+            return _db.SaveChangesAsync();
+
+
+        }
+    }
+}
---
+++ VideoTheque/Repositories/Film/IBluRayRepository.cs
@@ -0,0 +1,18 @@
+﻿using VideoTheque.DTOs;
+
+namespace VideoTheque.Repositories.Film
+{
+    public interface IBluRayRepository
+    {
+        Task<List<BluRayDto>> GetBluRays();
+        
+        ValueTask<BluRayDto?> GetBluRay(int id);
+        
+        Task InsertBluRay(BluRayDto film);
+        
+        Task UpdateBluRay(int id, BluRayDto film);
+        
+        Task DeleteBluRay(int id);
+        
+    }
+}
